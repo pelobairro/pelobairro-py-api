119d624e0927360a192dbb915bc557d6babc15fc
Pedro Renan <pedrorenan@gmail.com>
2021-07-03 12:20:07 +0000

    created functinos

--- download_data.py
+++ download_data.py
@@ -1,18 +1,21 @@
 import requests
 from pathlib import Path
-import shutil
+import pandas as pd
 
-path = "data"
 
-url = 'https://opendata.arcgis.com/api/v3/datasets/440b7424a6284e0b9bf11179b95bf8d1_1/downloads/data?format=csv&spatialRefId=4326'
 
-result = requests.get(url)
+def download_csv_geodados(url, filename):
+  path = "data"
+  result = requests.get(url)
+  Path(path).mkdir(parents=True, exist_ok=True)
+  filename = f'{path}/{filename}'
+  file = open(filename,'wb')
+  file.write(result.content)
+  file.close()
 
-Path(path).mkdir(parents=True, exist_ok=True)
-
-filename = f'{path}/estacionamentos.csv'
-
-file = open(filename,'wb')
-file.write(result.content)
-file.close()
-   
+# url = 'https://opendata.arcgis.com/api/v3/datasets/440b7424a6284e0b9bf11179b95bf8d1_1/downloads/data?format=csv&spatialRefId=4326'
+# download_csv_geodados(url, 'estacionamento_com_sem_hotspot.csv')
+# url = 'https://opendata.arcgis.com/api/v3/datasets/440b7424a6284e0b9bf11179b95bf8d1_1/downloads/data?format=csv&spatialRefId=4326'  
+url = 'https://geodados-cml.hub.arcgis.com/datasets/estacionamento-para-veloc%C3%ADpedes/explore?location=38.744511%2C-9.159595%2C12.81&showTable=true'
+html = pd.read_html(url)
+print(html)
